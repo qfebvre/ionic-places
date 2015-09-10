223b149c0b98d465f7e21e8365b129dbe13ee972
Quentin Febvre <Quentin@MacBook-Pro-de-Quentin.local>
2015-09-10 11:35:52 +0200

    friends lists

--- www/src/places/friends/friends.controller.js
+++ www/src/places/friends/friends.controller.js
@@ -1,6 +1,14 @@
 angular.module('places')
-	.controller('FriendsCtrl', function($scope, $ionicLoading,FriendsService) {
+	.controller('FriendsCtrl', function($scope, $ionicLoading,FriendsService, $http) {
 
+		$http.get("api/friends.json").
+		success(function(data,status,headers,config){
+			console.log(data)
 
+			$scope.list=data;
+		}).
 
+		error(function(data,status,headers,config){
+			console.log("error")
+		});
 	});
